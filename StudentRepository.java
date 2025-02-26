a294c5d167fd4c81bd0a6ceb692cbb807735e037
Rahul V <107417337+tcs-code@users.noreply.github.com>
2025-02-26 14:05:44 +0530

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.StudentGrader.Repository;
-
-import com.example.StudentGrader.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
